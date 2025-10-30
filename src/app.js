5b0810ffbe62fbceba5ef74d100b579a4991845e
elmanee <cp457920@gmail.com>
2025-10-30 07:18:54 -0600

    creacion de usairos y demas

--- src/app.js
+++ src/app.js
@@ -1,43 +1,29 @@
-// /home/agus/Documentos/VetHealth/VetBack/src/app.js
 const express = require('express');
 const app = express();
 const cors = require('cors'); 
+const path = require('path');
+
 // Middlewares
 app.use(express.json());
+app.use(cors({
+  origin: '*'
+}));
 
-// 1. Importar las rutas. Asegúrate de que las rutas son importadas como objetos
+// 1. Importar las rutas
 const productoRoutes = require('./routes/producto.routes');
-//const loteRoutes = require('./routes/loteProducto.routes');
 const citaRoutes = require('./routes/cita.routes'); 
 const clienteRoutes = require('./routes/cliente.routes');
 const expedienteRoutes = require('./routes/expediente.routes');
 const consultaRoutes = require('./routes/consulta.routes');
 const reporteRoutes = require('./routes/pdf.routes');
-const path = require('path');
 const proveedorRoutes = require('./routes/proveedor.routes');
 const categoriaRoutes = require('./routes/categoria.routes');
-const movimientoRoutes = require('./routes/movimiento.routes')
+const movimientoRoutes = require('./routes/movimiento.routes');
 const lotesRoutes = require('./routes/lote.routes');
 const authRoutes = require('./routes/auth.routes');
-<<<<<<< HEAD
-const usuarioRoutes = require('./routes/usuario.routes');
 
-=======
->>>>>>> 81fbbd2 (login con jwt y barrer)
-
-
-const expedienteRoutes = require('./routes/expediente.routes');
-const consultaRoutes = require('./routes/consulta.routes');
-const reporteRoutes = require('./routes/pdf.routes');
-const path = require('path');
-
-app.use(cors({
-  origin: '*'
-}));
-
-// conexion a las rutas
+// Conexión a las rutas
 app.use('/api/productos', productoRoutes); 
-//app.use('/api/lotes', loteRoutes);
 app.use('/api/citas', citaRoutes); 
 app.use('/api/clientes', clienteRoutes);
 app.use('/api/expedientes', expedienteRoutes);
@@ -47,13 +33,7 @@ app.use('/reportes', express.static(path.join(__dirname, 'reportes')));
 app.use('/api/proveedores', proveedorRoutes);
 app.use('/api/categorias', categoriaRoutes);
 app.use('/api/movimientos', movimientoRoutes);
-app.use('/api/lotes', lotesRoutes)
-app.use('/api/expedientes', expedienteRoutes);
-app.use('/api/consultas', consultaRoutes);
-app.use('/api/reportes', reporteRoutes);
-app.use('/reportes', express.static(path.join(__dirname, 'reportes')));
-
+app.use('/api/lotes', lotesRoutes);
+app.use('/api/auth', authRoutes);
 
 module.exports = app;
-
-
--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -4,18 +4,15 @@ require('dotenv').config();
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
-
+  
   if (!token) {
     return res.status(401).json({ message: 'Acceso denegado. No hay token.' });
   }
-
+  
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; 
-<<<<<<< HEAD
     console.log('token recibido y decodificado:', decoded);
-=======
->>>>>>> 81fbbd2 (login con jwt y barrer)
     next();
   } catch (err) {
     return res.status(403).json({ message: 'Token inválido o expirado.' });
